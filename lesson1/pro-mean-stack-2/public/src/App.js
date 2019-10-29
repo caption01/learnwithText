d783bcc6ccfd008427c333e8a41f59a6f1a292b4
nutchapon <nutchapon.han@gmail.com>
2019-10-29 21:58:06 +0700

    end lesson 1

--- lesson1/pro-mean-stack-2/public/src/App.js
+++
@@ -1,6 +0,0 @@
-const element = React.createElement("div", {
-  title: "Outer div"
-}, React.createElement("h1", null, " Hello World! "));
-
-
-ReactDOM.render(element, document.getElementById('content'));
